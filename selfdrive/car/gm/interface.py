66a6c0a0efc2bfd7d05364bbf11eabe9c58f9725
darknight111 <75706333+darknight111@users.noreply.github.com>
2022-01-11 21:35:39 -0800

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -61,7 +61,7 @@ class CarInterface(CarInterfaceBase):
 
     tire_stiffness_factor = 0.5
 
-    ret.minSteerSpeed = 11 * CV.KPH_TO_MS
+    ret.minSteerSpeed = 8 * CV.KPH_TO_MS
     ret.steerRateCost = 0.3625 # def : 2.0
     ret.steerActuatorDelay = 0.1925  # def: 0.2 Default delay, not measured yet
 

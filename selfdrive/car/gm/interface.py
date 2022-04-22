451ec27d55a516fad0bda46945ab82687d20a17f
darknight111 <75706333+darknight111@users.noreply.github.com>
2022-04-21 19:06:12 -0700

    Tune update. Long still needs work

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -73,10 +73,10 @@ class CarInterface(CarInterfaceBase):
     ret.centerToFront = ret.wheelbase * 0.49 # wild guess
     
     ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[10., 41.0], [10., 41.0]]
-    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.18, 0.26], [0.01, 0.02]]
+    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.18, 0.275], [0.01, 0.021]]
     ret.lateralTuning.pid.kdBP = [0.]
-    ret.lateralTuning.pid.kdV = [0.322]  # very sensitive to changes greater than 0.001
-    ret.lateralTuning.pid.kf = 0.0001
+    ret.lateralTuning.pid.kdV = [0.3]  # very sensitive to changes greater than 0.001
+    ret.lateralTuning.pid.kf = 0.0002
 
     # TODO: get actual value, for now starting with reasonable value for
     # civic and scaling by mass and wheelbase
@@ -88,11 +88,10 @@ class CarInterface(CarInterfaceBase):
                                                                          tire_stiffness_factor=tire_stiffness_factor)
 
     # longitudinal
-    ret.longitudinalTuning.kpBP = [0.0, 5.0, 10.0, 20.0, 35.0]
-    ret.longitudinalTuning.kpV = [0.6, 0.95, 1.19, 1.27, 1.18]
-  
-    ret.longitudinalTuning.kiBP = [0., 35.]
-    ret.longitudinalTuning.kiV = [0.31, 0.26]
+    ret.longitudinalTuning.kpBP = [0., 35.]
+    ret.longitudinalTuning.kpV = [0.18, 0.37] 
+    ret.longitudinalTuning.kiBP = [0., 35.] 
+    ret.longitudinalTuning.kiV = [0.22, 0.34]
     
     ret.longitudinalTuning.deadzoneBP = [0.]
     ret.longitudinalTuning.deadzoneV = [0.]
@@ -100,9 +99,9 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalActuatorDelayUpperBound = 0.15
     
     ret.startAccel = -0.8 # Required acceleraton to overcome creep braking
-    ret.stopAccel = -2.0 # Required acceleraton to keep vehicle stationary
-    ret.startingAccelRate = 0.9 # release brakes fast, brake_travel/s while releasing on restart
-    ret.stoppingDecelRate = 2.4 # reach stopping target smoothly, brake_travel/s while trying to stop
+    ret.stopAccel = -5.0 # Required acceleraton to keep vehicle stationary
+    ret.startingAccelRate = 0.17 # release brakes fast, brake_travel/s while releasing on restart
+    ret.stoppingDecelRate = 0.17 # reach stopping target smoothly, brake_travel/s while trying to stop
     ret.vEgoStopping = 0.5 # Speed at which the car goes into stopping state, when car starts requesting stopping accel
     ret.vEgoStarting = 0.5 # Speed at which the car goes into starting state, when car starts requesting starting accel, needs to be > or == vEgoStopping to avoid state transition oscillation
     ret.stoppingControl = True # Does the car allows full control even at lows speeds when stopping
